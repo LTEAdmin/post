b7abd9cd9090654a0465489d935e2e3deda1f8e5
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-23 21:09:31 -0300

    se traen todos los post, se construye la linea con formato lista solo falta colocar en Boldt el titulo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,17 @@
+const object = document.getElementById("traerPost");
+object.addEventListener("click", desafioPost());
 async function desafioPost() {
   const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
-
+  let construcPost = "";
   try {
     const respuesta = await posts.json();
     respuesta.forEach((element) => {
-      if (element.id < 21) { //aca se debe cambiar el formato por que se va a eniar al HTML como unalista usando vineta
-        console.log(`ID: ${element.id} , Titulo: ${element.title}`);
-      }
+      //aca se debe cambiar el formato por que se va a eniar al HTML como unalista usando vineta
+      construcPost = (` ${construcPost} <li><span font-weight="bold">${element.title}</span> \n ${element.body}</li> \n`);
     });
   } catch (error) {
     console.log(error);
   }
+  construcPost=(`<ul>${construcPost}</ul>`);
+  document.getElementById("post-data").innerHTML = construcPost;
 }
-desafioPost();
-/* 
-function mensaje() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("La información ha sido enviada.");
-    }, 3000);
-  });
-}
-
-async function mostrarMensaje(promesa) {
-  const resultado = await promesa;
-  console.log(resultado);
-}
-mostrarMensaje(mensaje()); */
